6338d9fde3a26771434faf0cbc6fcff1532aae3f
Strata Research <developers@strata.ltd>
2020-09-12 17:27:53 -0400

    feat: Added header, main & footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import data from './dino.json';
+
 function App() {
+  const [showForm, setHideForm] = useState(true);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submitted');
+
+    setHideForm(false);
+  };
+
   return (
     <div className='App'>
       <header>
@@ -9,32 +20,56 @@ function App() {
         <h1>Dinosaurs</h1>
         <h3>How do you compare?</h3>
       </header>
-      <form id='dino-compare'>
-        <div className='form-container'>
-          <p>Name:</p>
-          <input id='name' className='form-field__full' type='name' name='name' />
-          <p>Height</p>
-          <label>
-            Feet: <input id='feet' className='form-field__short' type='number' name='feet' />
-          </label>
-          <label>
-            inches: <input id='inches' className='form-field__short' type='number' name='inches' />
-          </label>
-          <p>Weight:</p>
-          <label>
-            <input id='weight' className='form-field__full' type='number' name='weight' />
-            lbs
-          </label>
-          <p>Diet:</p>
-          <select id='diet' className='form-field__full' name='diet'>
-            <option>Herbavor</option>
-            <option>Omnivor</option>
-            <option>Carnivor</option>
-          </select>
-          <div id='btn'>Compare Me!</div>
-        </div>
-      </form>
-      <main id='grid'></main>
+      <main className='main-content-container'>
+        {showForm && (
+          <form onSubmit={handleSubmit} id='dino-compare'>
+            <div className='form-container'>
+              <p>Name:</p>
+              <label className='visually-hidden' htmlFor='name'>
+                Name:
+              </label>
+              <input id='name' className='form-field__full' type='name' name='name' />
+              <p>Height</p>
+              <label htmlFor='feet'>
+                Feet: <input id='feet' className='form-field__short' type='number' name='feet' />
+              </label>
+              <label htmlFor='inches'>
+                inches:{' '}
+                <input id='inches' className='form-field__short' type='number' name='inches' />
+              </label>
+              <p>Weight:</p>
+              <label htmlFor='weight'>
+                <input id='weight' className='form-field__full' type='number' name='weight' />
+                lbs
+              </label>
+              <p>Diet:</p>
+              <label className='visually-hidden' htmlFor='diet'>
+                Diet:
+              </label>
+              <select id='diet' className='form-field__full' name='diet'>
+                <option>Herbavor</option>
+                <option>Omnivor</option>
+                <option>Carnivor</option>
+              </select>
+              <input className='submit-button' type='submit' name='submit' value='Compare Me!' />
+            </div>
+          </form>
+        )}
+        {!showForm && (
+          <div id='grid'>
+            {data.Dinos.map((obj) => (
+              <div className='grid-item'>
+                <h3>{obj.species}</h3>
+                <p>{obj.fact}</p>
+              </div>
+            ))}
+            <div className='grid-item'>
+              <h3>What is that?</h3>
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+          </div>
+        )}
+      </main>
       <footer>
         <p className='fine-print'>
           Data Sourced from <a href='https://www.wikipedia.org/'>Wikipedia.org</a>, all numbers are
